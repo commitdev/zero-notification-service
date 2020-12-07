2df781b7692890ee6b275f67de9ab13faae870fe
Bill Monkman <bmonkman@gmail.com>
2020-12-07 15:33:15 -0800

    Add sendgrid support for single and bulk mail (#5)

--- internal/service/api_email_service.go
+++ internal/service/api_email_service.go
@@ -6,9 +6,9 @@ import (
 	"net/http"
 
 	"github.com/commitdev/zero-notification-service/internal/config"
+	"github.com/commitdev/zero-notification-service/internal/mail"
 	"github.com/commitdev/zero-notification-service/internal/server"
 	"github.com/sendgrid/sendgrid-go"
-	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 // EmailApiService is a service that implents the logic for the EmailApiServicer
@@ -24,16 +24,49 @@ func NewEmailApiService(c *config.Config) server.EmailApiServicer {
 }
 
 // SendEmail - Send an email
-func (s *EmailApiService) SendEmail(ctx context.Context, emailMessage server.EmailMessage) (server.ImplResponse, error) {
-	from := mail.NewEmail(emailMessage.To.Name, emailMessage.To.Address)
-	to := mail.NewEmail(emailMessage.From.Name, emailMessage.From.Address)
-	message := mail.NewSingleEmail(from, emailMessage.Subject, to, emailMessage.Body, emailMessage.RichBody)
+func (s *EmailApiService) SendEmail(ctx context.Context, sendMailRequest server.SendMailRequest) (server.ImplResponse, error) {
 	client := sendgrid.NewSendClient(s.config.SendgridAPIKey)
-	response, err := client.Send(message)
-	fmt.Printf("%v", response)
+	response, err := mail.SendIndividualMail(sendMailRequest.To, sendMailRequest.From, sendMailRequest.Message, client)
+
 	if err != nil {
+		fmt.Printf("Error sending mail: %v\n", response)
 		return server.Response(http.StatusInternalServerError, nil), fmt.Errorf("Unable to send email: %v", err)
 	}
 
-	return server.Response(http.StatusOK, server.SendMailResponse{}), nil
+	if !(response.StatusCode >= 200 && response.StatusCode <= 299) {
+		fmt.Printf("Failure from Sendgrid when sending mail: %v\n", response)
+		return server.Response(http.StatusInternalServerError, nil), fmt.Errorf("Unable to send email: %v from mail provider: %v", response.StatusCode, response.Body)
+	}
+
+	return server.Response(http.StatusOK, server.SendMailResponse{response.Headers["X-Message-Id"][0]}), nil
+}
+
+// SendBulk - Send a batch of emails to many users with the same content. Note that it is possible for only a subset of these to fail.
+func (s *EmailApiService) SendBulk(ctx context.Context, sendBulkMailRequest server.SendBulkMailRequest) (server.ImplResponse, error) {
+	client := sendgrid.NewSendClient(s.config.SendgridAPIKey)
+
+	responseChannel := make(chan mail.BulkSendAttempt)
+
+	mail.SendBulkMail(sendBulkMailRequest.ToAddresses, sendBulkMailRequest.From, sendBulkMailRequest.Message, client, responseChannel)
+
+	var successful []server.SendBulkMailResponseSuccessful
+	var failed []server.SendBulkMailResponseFailed
+
+	// Read all the responses from the channel. This will block if responses aren't ready and the channel is not yet closed
+	for r := range responseChannel {
+		if r.Error != nil {
+			fmt.Printf("Error sending bulk mail: %v", r.Error)
+			failed = append(failed, server.SendBulkMailResponseFailed{r.EmailAddress, fmt.Sprintf("Unable to send email: %v\n", r.Error)})
+		} else if !(r.Response.StatusCode >= 200 && r.Response.StatusCode <= 299) {
+			fmt.Printf("Failure from Sendgrid when sending bulk mail: %v", r.Response)
+			failed = append(failed, server.SendBulkMailResponseFailed{r.EmailAddress, fmt.Sprintf("Unable to send email: %v from mail provider: %v\n", r.Response.StatusCode, r.Response.Body)})
+		} else {
+			successful = append(successful, server.SendBulkMailResponseSuccessful{r.EmailAddress, r.Response.Headers["X-Message-Id"][0]})
+		}
+	}
+	responseCode := http.StatusOK
+	if len(successful) == 0 {
+		responseCode = http.StatusInternalServerError
+	}
+	return server.Response(responseCode, server.SendBulkMailResponse{successful, failed}), nil
 }
